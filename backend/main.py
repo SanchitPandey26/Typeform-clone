fbae5783b7d696c297657aa0960f1cab22d01a2c
SanchitPandey26 <sanchitpandey263@gmail.com>
2026-07-27 13:39:20 +0530

    debug deployement

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
-from app.main import app
+try:
+    from app.main import app
+except Exception as e:
+    import traceback
+    from fastapi import FastAPI
+    from fastapi.responses import PlainTextResponse
+    
+    app = FastAPI()
+    err_traceback = traceback.format_exc()
+    
+    @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"])
+    def catch_all(path: str):
+        return PlainTextResponse(f"Initialization Error:\n\n{err_traceback}", status_code=500)
